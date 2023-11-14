f21d232d8b56f1473d836519d3b3a87eaa246b3f
JoggerChe <120989445+BorovChe@users.noreply.github.com>
2023-11-14 20:32:14 +0200

    push

---
+++ models/contact.js
@@ -0,0 +1,48 @@
+const {Schema, model} = require('mongoose');
+const {handleMongooseError} = require('../helpers')
+const Joi = require("joi");
+
+const bookSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+      favorite: {
+        type: Boolean,
+        default: false,
+      },
+});
+
+bookSchema.post("save", handleMongooseError)
+
+const addSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string().min(3).max(14).required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchemas = Joi.object({
+  favorite: Joi.boolean().required(),
+})
+
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchemas,
+}
+
+const Contact = model('contact', bookSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,18 +4,21 @@ const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middllewares");
+const { validateBody, isValidId } = require("../../middllewares");
 
-const schemas = require("../../schemas/contacts");
+const {schemas} = require("../../models/contact");
 
 router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:id", ctrl.deleteContact);
+router.delete("/:id", isValidId, ctrl.deleteContact);
 
-router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContact);
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
+
+router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchemas), ctrl.updateStatusContact);
+
 
 module.exports = router;
